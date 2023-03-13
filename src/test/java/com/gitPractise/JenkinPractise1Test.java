da4ff8e5e6dae7b6b4ca8ef7fea2607c6d64a187
deepak <patilrhdeepak@gmail.com>
2023-03-13 13:00:57 +0530

    first time project is added to global repo

---
+++ src/test/java/com/gitPractise/JenkinPractise1Test.java
@@ -0,0 +1,21 @@
+package com.gitPractise;
+
+import org.testng.annotations.Test;
+
+public class JenkinPractise1Test {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("class1---->test1");
+		
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("class1---->test2");
+		
+	}
+	
+}
---
+++ src/test/java/com/gitPractise/JenkinPractise2Test.java
@@ -0,0 +1,19 @@
+package com.gitPractise;
+
+import org.testng.annotations.Test;
+
+public class JenkinPractise2Test {
+
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("class2--->test1");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("class2--->test2");
+	}
+
+}
